afade96833f1a82ba46f849e98e2d2d4a4106c25
TamNhien <canhthien91@gmail.com>
2024-03-18 21:58:04 +0700

    Add project files.

---
+++ P_TinhTienVonValai.py
@@ -0,0 +1,12 @@
+﻿def main():
+    T = float(input("Nhập số tiền gửi ban đầu (triệu đồng) : "))
+    print("Năm\tTiền vốn (triệu)\tLãi (triệu)")
+
+    for nam in range(1, 16):
+        tien_von = T * (1 + 0.08) ** nam
+        tien_lai = T * 0.08 * nam
+        print(f"{nam}\t{tien_von:.2f}\t\t\t{tien_lai:.2f}")
+
+if __name__ == "__main__":
+    main()
+
